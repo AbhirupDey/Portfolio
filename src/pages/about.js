9d4f4cf872721b2aa7429d80a6beccf8fd0c809b
AbhirupDey <deyabhirup2004@gmail.com>
2025-06-14 01:12:43 +0530

    Fix ESLint errors: escape apostrophes in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -53,9 +53,9 @@ const about = () => {
             <div className='col-span-3 flex flex-col items-start justify-start'>
               <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>Biography</h2>
 
-              <p className='font-medium'>Hi, I'm <b>Abhirup Dey</b>, a full-stack developer from India and currently a final-year undergraduate pursuing a B.Tech in Computer Science at the Institute of Engineering and Management (IEM), Kolkata.</p>
-              <p className='my-4 font-medium'>I believe that design is about more than just making things look pretty – it's about solving problems and creating intuitive, enjoyable experiences for users.</p>
-              <p className='font-medium'>Whether I'm working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
+              <p className='font-medium'>Hi, I&apos;m <b>Abhirup Dey</b>, a full-stack developer from India and currently a final-year undergraduate pursuing a B.Tech in Computer Science at the Institute of Engineering and Management (IEM), Kolkata.</p>
+              <p className='my-4 font-medium'>I believe that design is about more than just making things look pretty – it&apos;s about solving problems and creating intuitive, enjoyable experiences for users.</p>
+              <p className='font-medium'>Whether I&apos;m working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
 
             </div>
             <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark
